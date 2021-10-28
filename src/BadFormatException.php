9d307e4a198f32dbe8f71a90d869aa1a91986cd9
mpyw <ryosuke_i_628@yahoo.co.jp>
2021-10-28 16:34:32 +0900

    Refactor: Remove verbose __construct() override

--- src/BadFormatException.php
+++ src/BadFormatException.php
@@ -4,8 +4,4 @@ namespace Mpyw\UuidUlidConverter;
 
 final class BadFormatException extends \RuntimeException implements ConversionException
 {
-    public function __construct(string $message)
-    {
-        parent::__construct($message);
-    }
 }
